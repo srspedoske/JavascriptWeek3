06b0c41fc075a86d3742ca963d9e3181e0a20a3b
Sally Spedoske <sachai06@gmail.com>
2018-10-20 12:35:47 -0500

    added the necessary files this time

---
+++ index.js
@@ -0,0 +1,9 @@
+//Install package via npm install
+//load package with require
+var _ = require('lodash');
+
+//use package based on variable we import with
+for(var i = 0; i<100; i++){
+var r = _.random(10);
+console.log(r);}
+
